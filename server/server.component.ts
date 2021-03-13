176a6c59788b23e3c477f3e0fce332149254fdfe
Amanda-Katherine <walker.akw@gmail.com>
2021-03-12 21:56:02 -0500

    set up files for new server component

---
+++ server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server', //created an HTML selector which is usable in .html files 
+    templateUrl: './server.component.html' //tells the html that should be run for this typescript file
+})
+
+export class ServerComponent {
+
+}
